f1b2eb540f63a82d0eb75d78f78dc4e95ff62102
fyx <fyx@qq.com>
2019-09-22 12:19:27 +0800

    2019_9_22

---
+++ firstProject/public/js/footer.js
@@ -0,0 +1,12 @@
+$(function(){
+    $.ajax({
+        url:'footer.html',
+        type:'get',
+        success:function(result){
+    //result拿到的是herder.html的代码块 
+           $(result).replaceAll('footer')
+    //因为header.html有样式，所以添加样式代码到主页上
+            $('<link rel="stylesheet" href="css/footer.css">').appendTo('head')
+        }
+    })
+    })
